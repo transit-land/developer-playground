60bd90a101a168779c314e2892ad38510ed3f428
Meghan <mmhade@gmail.com>
2015-06-17 15:35:50 -0700

    remove uneeded event tracking functions

--- js/views/grid.js
+++ js/views/grid.js
@@ -3,11 +3,6 @@ var DeveloperPlayground = DeveloperPlayground || {};
 DeveloperPlayground.GridView = Backbone.View.extend({
     el: '.backgrid-container',
 
-    events: {
-        'mouseover .backgrid-container' : 'mouseoverGrid',
-        'click .backgrid-container' : 'clickGrid'
-    },
-
     initialize:function(options){
         this.collection = options.collection;
         this.listenTo(this.collection, 'sync', this.render);
@@ -134,14 +129,6 @@ DeveloperPlayground.GridView = Backbone.View.extend({
             });
             $("#results").append(grid.render().$el);
         }
-    },
-
-    mouseoverGrid: function(){
-        ga('send', 'event', 'grid', 'mouseover');
-    },
-
-    clickGrid: function(){
-        ga('send', 'event', 'grid', 'click');
     },
 
     close: function() {
